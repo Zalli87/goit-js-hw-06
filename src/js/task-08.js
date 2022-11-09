4b01a6a9b4f1d7623df2cdbdc97ca648c5123130
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2022-11-09 21:04:07 +0200

    добавляє виконання 8-го завдання

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form')
+
+form.addEventListener('submit', onSubmitClick);
+
+function onSubmitClick (event) {
+    event.preventDefault();
+    const email = event.currentTarget.email.value;
+    const password = event.currentTarget.password.value;
+    if (!event.currentTarget.password.value) {
+        alert('Все поля должны быть заполнены...');
+        event.currentTarget.reset();
+        return
+    }
+    const data = {
+        email,
+        password
+    }
+
+    event.currentTarget.reset();
+    console.log(data);
+    
+}
 
