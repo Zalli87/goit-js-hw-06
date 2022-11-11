f837a6ba5a9be59f27b00edc1fe34c998fa82e71
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2022-11-11 16:12:18 +0200

    додає виконання 9-го завдання

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const colorTitle = document.querySelector('.color');
+
+const changeColorBtn = document.querySelector('.change-color');
+
+changeColorBtn.addEventListener('click', onChangeColorBtnClick);
+
+function onChangeColorBtnClick() {
+  colorTitle.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
 }
